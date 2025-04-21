3c7566bb6ce66409b6b5de61a24d9050fa4f3ec6
joel santos <santoaprenderdb@gmail.com>
2025-04-21 22:47:24 +0200

    run deploy commit

--- ScapeWindows/src/body.jsx
+++ ScapeWindows/src/body.jsx
@@ -4,7 +4,67 @@ function BodySW(){
     
     return(
         <div className="bodySW">
-            <h1>scroll 4</h1>
+            <div className="pared">
+                <div className="window-left">
+                    <div className="innerWindow">
+                        <div className="line-1"></div>
+                        <div className="line-2"></div>
+                    </div>
+                </div>
+                <div className="window-right">
+                    <div className="innerWindow">
+                        <div className="line-1"></div>
+                        <div className="line-2"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="suelo">
+                <div className="innerSuelo">
+                    <div className="item"></div>
+                    <div className="item"></div>
+                    <div className="item"></div>
+                    <div className="item"></div>
+                    <div className="item"></div>
+                </div>
+                <div className="innerSuelo">
+                    <div className="item-1"></div>
+                    <div className="item-2"></div>
+                    <div className="item-3"></div>
+                    <div className="item-4"></div>
+                    <div className="item-5"></div>
+                    <div className="item-6"></div>
+                </div>
+                <div className="innerSuelo">
+                    <div className="item"></div>
+                    <div className="item"></div>
+                    <div className="item"></div>
+                    <div className="item"></div>
+                    <div className="item"></div>
+                </div>
+                <div className="innerSuelo">
+                    <div className="item-1"></div>
+                    <div className="item-2"></div>
+                    <div className="item-3"></div>
+                    <div className="item-4"></div>
+                    <div className="item-5"></div>
+                    <div className="item-6"></div>
+                </div>
+                <div className="innerSuelo">
+                    <div className="item"></div>
+                    <div className="item"></div>
+                    <div className="item"></div>
+                    <div className="item"></div>
+                    <div className="item"></div>
+                </div>
+                <div className="innerSuelo">
+                    <div className="item-1"></div>
+                    <div className="item"></div>
+                    <div className="item"></div>
+                    <div className="item"></div>
+                    <div className="item"></div>
+                    <div className="item-6"></div>
+                </div>
+            </div>
         </div>
     )
 }
