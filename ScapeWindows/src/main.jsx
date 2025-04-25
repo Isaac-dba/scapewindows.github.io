9faf0f582e5f2b0f1585e5997a4059299dcadeed
joel santos <santoaprenderdb@gmail.com>
2025-04-25 19:25:38 +0200

    reset

--- ScapeWindows/src/main.jsx
+++ ScapeWindows/src/main.jsx
@@ -1,14 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
-import Navbar from './header'
-import FooterSW from './footer'
-import BodySW from './body'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navbar />
-    <BodySW />
-    <FooterSW />
+    
   </StrictMode>,
 )
--- ScapeWindows/src/niveles/nivel0.jsx
+++
@@ -1,6 +0,0 @@
-export const Nivel0 = () => {
-
-    return(
-        <h1>nivel 0</h1>
-    )
-}
