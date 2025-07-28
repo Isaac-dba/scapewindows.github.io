fbda38f5a582986961e1d7a7b09f07fbfd95ade7
joel santos <santoaprenderdb@gmail.com>
2025-07-28 17:12:54 +0200

    ajustes finales

--- ScapeWindows/src/componets/article1.jsx
+++ ScapeWindows/src/componets/article1.jsx
@@ -6,9 +6,9 @@ export function Article1({setIndex}){
     //evento click
     const [clicked, setClicked] = useState(null);
     //generar camino
+    console.log(clicked);
     const [path, setPath] = useState([]);
     useEffect(() => {setPath(generatPath())}, [setIndex]);
-    
     //state de los clicks
     const [trueClicks, setTrueClicks] = useState([]);
     const [falseClicks, setFalseClicks] = useState([]);
@@ -19,7 +19,6 @@ export function Article1({setIndex}){
         audioRef.current = new Audio(import.meta.env.BASE_URL + "/sound/ticking-clock-background-sound-effect-331707.mp3");
     }, []);
     //verificar camino
-    console.log(clicked);
     const checkedPath = (i) => path.includes(i);
     const contentReloj = [
         <Reloj path={[path[0]]} />,
@@ -51,26 +50,21 @@ export function Article1({setIndex}){
             <div className={`floor ${trueClicks.length == 10 ? "winFloor" : ""}`}>
                 {Array.from({length: 50}, (_, i) => (
                     <div key={i} className={`square square${i} ${trueClicks.length == 10 ? "win": ""}`} 
-                    
                     onClick={() => {
-                        if(checkedPath(i)){
-                            if(clicked === i){
-                                return;
-                            }else{
+                            // Evita repetir si ya se ha hecho clic en este cuadro
+                            if (trueClicks.includes(i) || falseClicks.includes(i)) return;
+                            if (checkedPath(i)) {
                                 document.querySelector(`.square${i}`).classList.add("true");
-                            }
-                            setTrueClicks((prev) => [...prev, "true"]);
-                        }else{
-                            if(clicked === i){
-                                return;
-                            }else{
+                                setTrueClicks((prev) => [...prev, i]);
+                            } else {
                                 document.querySelector(`.square${i}`).classList.add("false");
-                            }
-                            setFalseClicks((prev) => [...prev, "false"]);
-                            audioRef.current?.play();
-                        }
-                        setClicked(i);
-                    }}> {clicked === null ? 0 : clicked + 1} 
+                                setFalseClicks((prev) => [...prev, i]);
+                                audioRef.current?.play();
+                            } 
+                            setClicked(i);
+                        }}
+                    >
+                    {/* {clicked === null ? 0 : clicked + 1}  */}
                     </div>
                 ))}
                 <button id="btn-1" onClick={() => handleNext({setIndex})}
