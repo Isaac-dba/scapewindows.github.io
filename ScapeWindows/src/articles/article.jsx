2e9319f911900be0bf38fa7bbaa932b8a36a0cd7
joel santos <santoaprenderdb@gmail.com>
2026-04-29 12:06:13 +0200

    remover

--- ScapeWindows/src/articles/article.jsx
+++ ScapeWindows/src/articles/article.jsx
@@ -2,10 +2,10 @@ import { resetNivel } from "../logic/constant";
 
 export const Article = ({id, contenido, setIndex}) => {
 
-    return(
+    return (
         <article className={`articulo`} id={id}>
             <div className={`divContent`}>{contenido}</div>
-            {/* <button onClick={() => resetNivel({setIndex})} >reset</button> */}
+            {/*<button onClick={() => resetNivel({setIndex})} >reset</button>*/}
             {/* <button onClick={() => window.location.reload()} >reiniciar</button> */}
 
         </article>
